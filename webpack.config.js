a7c46838cb8ecd449e827bff5a66f8688d4c2e1a
Jonathan Green <dev.jonathan.green@gmail.com>
2022-11-01 10:24:31 -0400

    changed folders names

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ const canisterEnvVariables = initCanisterEnv();
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-const frontendDirectory = "dip721_communitycoin_assets";
+const frontendDirectory = "ui";
 
 const asset_entry = path.join("src", frontendDirectory, "src", "index.html");
 
