5d7eadceb00b2a440740dd7bd81ec6db867ec236
cryptoisgood <daniel@cryptoisgood.co>
2022-11-11 13:59:14 -0500

    fix bug

--- src/ui/src/components/roadmap.tsx
+++ src/ui/src/components/roadmap.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Button, Col, Container, Navbar, Row } from "react-bootstrap";
-import { PieChart, Pie, LabelList, ResponsiveContainer } from "recharts";
 
 const RoadMap = () => {
 
