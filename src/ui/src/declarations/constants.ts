73e83d2c42a6136d72a678cb08cb14a832af8abf
Remco <remco@r3m.codes>
2022-11-12 23:41:16 +0100

    add whitelisted canisters

--- src/ui/src/declarations/constants.ts
+++ src/ui/src/declarations/constants.ts
@@ -3,7 +3,13 @@ export const treasuryCanisterId = "unwqb-kyaaa-aaaak-ac5aa-cai";
 export const coinCanisterId = "5gxp5-jyaaa-aaaag-qarma-cai";
 export const distributionCanisterId = "cjzlc-riaaa-aaaal-qbgwa-cai";
 export const wicpCanisterId = "utozz-siaaa-aaaam-qaaxq-cai";
-export const whiteListedCanister = [daoCanisterId, coinCanisterId];
+export const whiteListedCanister = [
+  daoCanisterId,
+  coinCanisterId,
+  treasuryCanisterId,
+  distributionCanisterId,
+  wicpCanisterId,
+];
 export const icpHost = "https://ic0.app";
 
 export default {
